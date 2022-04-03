a01507974a20329d5a61e5354f4a84db851cdb25
mark-lzm <lizhiming596@126.com>
2022-04-03 11:40:38 +0800

    springmvc基于注解实现请求分发

--- springmvc/src/main/java/net/litchi/springmvc/web/controller/HelloController.java
+++ springmvc/src/main/java/net/litchi/springmvc/web/controller/HelloController.java
@@ -1,17 +1,30 @@
 package net.litchi.springmvc.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 /**
+ * 1.请求分发
+ * 2.接收参数
+ * 3、返回响应
+ *
  * @Description
  * @Author Mark
  * @Date 2022/4/3 10:48
  */
-public class HelloController implements Controller {
-    @Override
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
-        System.out.println("hello controller");
-        return null;
+@Controller
+@RequestMapping("/hello")
+public class HelloController{
+
+    @RequestMapping("/1")
+    public String hello1(){
+        System.out.println("hello1");
+        return "success";
+    }
+
+    @RequestMapping("/2")
+    public void hello2(){
+        System.out.println("hello2");
     }
 }
