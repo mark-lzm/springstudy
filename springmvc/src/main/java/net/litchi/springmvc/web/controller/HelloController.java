a9021a333f08710192529843bc77381f15e5b0fb
mark-lzm <lizhiming596@126.com>
2022-04-03 14:35:47 +0800

    springmvc-接收参数@RequestParam

--- springmvc/src/main/java/net/litchi/springmvc/web/controller/HelloController.java
+++ springmvc/src/main/java/net/litchi/springmvc/web/controller/HelloController.java
@@ -1,8 +1,17 @@
 package net.litchi.springmvc.web.controller;
 
+import lombok.AllArgsConstructor;
+import net.litchi.springmvc.web.controller.pojo.bo.TestBo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 1.请求分发
@@ -15,16 +24,56 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @RequestMapping("/hello")
-public class HelloController{
+@AllArgsConstructor
+public class HelloController {
+
+    /**
+     *
+     */
+    private HttpServletRequest request;
+    private HttpServletResponse response;
 
     @RequestMapping("/1")
-    public String hello1(){
+    public void hello1() {
         System.out.println("hello1");
-        return "success";
     }
 
     @RequestMapping("/2")
-    public void hello2(){
+    public void hello2() {
         System.out.println("hello2");
     }
+
+    @RequestMapping("/3")
+    public void hello3() {
+        /**
+         * 1.使用request接收参数，接收的都是String类型
+         */
+        request.getParameterMap().keySet().forEach(key -> {
+            System.out.println(key + " : " + request.getParameterMap().get(key));
+        });
+    }
+
+    /**
+     * 2.Spring接收参数，类型不对会报bug
+     */
+    @RequestMapping("/4")
+    public void hello4(@RequestParam(name = "name", /*required = true,*/ defaultValue = "litchi") String username, String password, int age, double money,
+                       String[] checkbox,
+                       @RequestParam Map<String, String[]> params,
+                       TestBo bo) {
+        params.keySet().forEach(key->{
+            System.out.println(key + ":" + params.get(key));
+        });
+
+        System.out.println(bo);
+
+//        System.out.println(checkbox);
+
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(age);
+        System.out.println(money);
+    }
+
+
 }
